51de7a2540ea387d71f9ff3ab5c28c1a3c03540a
LLIJIYAPNIK <kas4wot@yandex.ru>
2026-07-12 12:41:35 +0300

    refactor: remove redundant participant and character states

--- app/domain/game/__init__.py
+++ app/domain/game/__init__.py
@@ -1,5 +1,4 @@
 from .entity import Game
-from .participant import Participant, ParticipantState
 from .state import GameState
 
-__all__ = ["Game", "GameState", "Participant", "ParticipantState"]
+__all__ = ["Game", "GameState"]
